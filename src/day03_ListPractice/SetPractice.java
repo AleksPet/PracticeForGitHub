340950cbb9994dfd327fed248224c5997a86acf8
AleksPet <p.aleksandra83@gmail.com>
2021-07-29 14:43:54 -0400

    Practice #2Practice second commit

---
+++ src/day03_ListPractice/SetPractice.java
@@ -0,0 +1,8 @@
+package day03_ListPractice;
+
+public class SetPractice {
+    public static void main(String[] args) {
+        //This is second task for merge practising
+        //adding more info to commit again
+    }
+}
