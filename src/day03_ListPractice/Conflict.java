d96d996e810d2e29e8853db0dc5498135229f427
AleksPet <p.aleksandra83@gmail.com>
2021-07-30 10:43:30 -0400

    conflict 2

--- src/day03_ListPractice/Conflict.java
+++ src/day03_ListPractice/Conflict.java
@@ -3,6 +3,8 @@ package day03_ListPractice;
 public class Conflict {
 
     public static void main(String[] args) {
+
+        //MORE WORK ON COLLECTION 2
 //this is my final version
         //I can type anything here
         // We are about to resolve conflict
