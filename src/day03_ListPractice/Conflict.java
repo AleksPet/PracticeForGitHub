9799759d546b20377f3f1ba48e281112b2660c93
AleksPet <p.aleksandra83@gmail.com>
2021-07-30 10:45:42 -0400

    conflict ADD NEW WORDS 2

--- src/day03_ListPractice/Conflict.java
+++ src/day03_ListPractice/Conflict.java
@@ -3,6 +3,7 @@ package day03_ListPractice;
 public class Conflict {
 
     public static void main(String[] args) {
+        //tHIS IS THE CHANGE COLLECTION 2 DOES NOT KNOW ABOUT
 //this is my final version
         //I can type anything here
         // We are about to resolve conflict
